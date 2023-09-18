9ba3af08d9a915ecede23897b125863d30262c05
unknown <adityabanka1947@gmail.com>
2023-09-18 12:41:21 +0530

    newServer Added

---
+++ newServer.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <thread>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+using namespace std;
+
+class Server
+{
+public:
+    string IPAddress;
+    int portNumber;
+
+    int socketFD;
+    struct sockaddr_in address;
+    socklen_t length = sizeof(address);
+
+    void takeInput()
+    {
+        cout << "Enter IP address (0 for localhost:8000).. ";
+        getline(cin, IPAddress);
+        if (IPAddress.compare("0") == 0)
+        {
+            IPAddress = "127.0.0.1";
+            portNumber = 8000;
+        }
+        else
+        {
+            cout << "Enter port number.. ";
+            cin >> portNumber;
+        }
+    }
+
+    int createServer()
+    {
+        address.sin_family = AF_INET;
+        address.sin_port = htons(portNumber);
+        address.sin_addr.s_addr = inet_addr(IPAddress.c_str());
+
+        cout << "1. Creation\t";
+        socketFD = socket(AF_INET, SOCK_STREAM, 0);
+        if (socketFD == -1)
+        {
+            cout << "Failed\n";
+            return (-1);
+        }
+        cout << "Successful\n";
+
+        cout << "2. Binding\t";
+        int binding = bind(socketFD, (struct sockaddr *)&address, length);
+        if (binding == -1)
+        {
+            cout << "Failed\n";
+            return (-1);
+        }
+        cout << "Successful\n";
+
+        cout << "3. Listening\t";
+        int listening = listen(socketFD, 10);
+        if (listening == -1)
+        {
+            cout << "Failed\n";
+            return (-1);
+        }
+        cout << "Successful\n";
+
+        cout << "4. Creation\tSuccessful\n";
+        return (1);
+    }
+
+} server;
+
+class Client
+{
+public:
+    string name;
+    int index;
+
+    int is_admin;
+    int is_bot;
+
+    int socketFD;
+    struct sockaddr address;
+    socklen_t length;
+
+    pthread_t initializerThread;
+};
+vector<Client> clients;
+vector<Client> processingClients;
+
+struct message
+{
+    string sender;
+    string receiver;
+
+    string message;
+    string metadata;
+};
+
+void accepter();
+void initializer();
+int serchClient(string name);
+
+int main()
+{
+    server.takeInput();
+    int counter = 0, check;
+    do
+    {
+        counter++;
+        check = server.createServer();
+    } while (counter != 5 && check == -1);
+    if (check == -1)
+    {
+        cout << "Failed to create the server, retry later\n";
+        return (0);
+    }
+    cout << "\n5. Threading\n   accepter()\t";
+}
+
+void accepter()
+{
+    cout << "Successful\n\n";
+    cout << "Server is accepting connections..\n\n";
+    while (1)
+    {
+        Client *client = (Client *)malloc(sizeof(Client));
+        client->socketFD = accept(server.socketFD, &(client->address), &(client->length));
+        cout << "New client connected : sent for processing\n";
+        processingClients.push_back(*client);
+
+        // thread thread1(threadFunction);
+        // thread1.join();
+    }
+}
+
+void initializer()
+{
+    Client client = processingClients.back();
+    processingClients.pop_back();
+}
